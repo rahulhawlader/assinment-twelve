4f49c1c31cb3caba196c765e4d1cd5db06dbaffc
Rahulhawlader <hawladerrahul8@gmail.com>
2022-05-22 16:44:57 +0600

    products json

--- src/Pages/Home/Benner/Benner.js
+++ src/Pages/Home/Benner/Benner.js
@@ -10,7 +10,9 @@ const Benner = () => {
                 <div>
                     <h1 class="text-5xl font-bold">Shop Bike Racks</h1>
                     <p class="py-6">Bike fitting is the process that involves pairing a rider to their bike to attain the best performance while minimising any chance of injury. Cycling is a very low impact activity but even so, problems can arise if you're not set up correctly</p>
-                    <button class="btn btn-primary">Get Started</button>
+                    <button type="button" class="btn bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-black font-bold ...">
+                        get Start
+                    </button>
                 </div>
             </div>
         </div>
