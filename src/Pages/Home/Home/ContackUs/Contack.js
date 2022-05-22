2969ef7efe7d8abdda248af1b96218b1f853f614
Rahulhawlader <hawladerrahul8@gmail.com>
2022-05-23 04:39:55 +0600

    contack

--- src/Pages/Home/Home/ContackUs/Contack.js
+++ src/Pages/Home/Home/ContackUs/Contack.js
@@ -1,36 +1,36 @@
-// import React from 'react';
+import React from 'react';
 
-// const Contack = () => {
-//     return (
-//         <div className='bg-primary px-10 py-14 mt-20'>
-//             <div className='text-center pb-14 text-white'>
-//                 <p className='text-xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary'>
-//                     Contact Us
-//                 </p>
-//                 <h1 className='text-4xl'>Stay connected with us</h1>
-//             </div>
-//             <div className='grid grid-cols-1 justify-items-center gap-5'>
-//                 <input
-//                     type='text'
-//                     placeholder='Email Address'
-//                     className='input w-full max-w-md'
-//                 />
-//                 <input
-//                     type='text'
-//                     placeholder='Subject'
-//                     className='input w-full max-w-md'
-//                 />
-//                 <textarea
-//                     className='textarea w-full max-w-md'
-//                     placeholder='Your message'
-//                     rows={6}
-//                 ></textarea>
-//                 <button type="button" class="btn bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-black font-bold ...">
-//                     get Start
-//                 </button>
-//             </div>
-//         </div>
-//     );
-// };
+const Contack = () => {
+    return (
+        <div className='bg-primary px-10 py-14 mt-20'>
+            <div className='text-center pb-14 text-white'>
+                <p className='text-xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary'>
+                    Contact Us
+                </p>
+                <h1 className='text-4xl'>Stay connected with us</h1>
+            </div>
+            <div className='grid grid-cols-1 justify-items-center gap-5'>
+                <input
+                    type='text'
+                    placeholder='Email Address'
+                    className='input w-full max-w-md'
+                />
+                <input
+                    type='text'
+                    placeholder='Subject'
+                    className='input w-full max-w-md'
+                />
+                <textarea
+                    className='textarea w-full max-w-md'
+                    placeholder='Your message'
+                    rows={6}
+                ></textarea>
+                <button type="button" class="btn bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-black font-bold ...">
+                    get Start
+                </button>
+            </div>
+        </div>
+    );
+};
 
-// export default Contack;
+export default Contack;
