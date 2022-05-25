4db0b40aac4404990b9c2fa6803dfc491d2e9390
Rahulhawlader <hawladerrahul8@gmail.com>
2022-05-25 15:51:23 +0600

    token

--- src/Pages/DashBoard/MyOrders.js
+++ src/Pages/DashBoard/MyOrders.js
@@ -8,7 +8,12 @@ const MyOrders = () => {
 
     useEffect(() => {
         if (user) {
-            fetch(`http://localhost:5000/order?email=${user.email}`)
+            fetch(`http://localhost:5000/order?email=${user.email}`, {
+                method: 'GET',
+                headers: {
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
 
                 .then(res => res.json())
                 .then(data => setOrders(data))
